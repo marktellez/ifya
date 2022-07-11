a2ae12e3272ac3b46446ba925bc11fd55ed8593a
Mark Tellez <mark@devmentor.live>
2022-07-11 11:02:24 -0500

    favicon

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,14 @@
+import Head from "next/head";
 import "./globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
